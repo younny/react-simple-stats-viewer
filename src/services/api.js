1993abed6807331d2b1091edc19569329ceebd41
Dooyoung Gi <dooyounny@gmail.com>
2018-12-10 21:28:31 +0900

    Refine error handling

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,31 @@
+/* eslint-env browser */
+/* eslint no-alert: 0 */
+
 import axios from 'axios'
 
 const BASE_URL = ' https://api.pro.coinbase.com'
+
+/* Show error alert */
+const showAlert = ({ data, status }) => {
+  window.alert(
+    `status: ${status}\n${JSON.stringify(data)}`
+  )
+}
 
 /*
  Coinbase API services
 */
 
 const getProducts = (onSuccess, onError) => axios.get(`${BASE_URL}/products`)
-  .then(response => onSuccess(response.data))
-  .catch((error) => {
-    console.log(error)
-    onError(error)
-  })
+  .then(({ data }) => onSuccess(data))
+  .catch(({ response }) => onError(response))
 
-const getStats = (query, onSuccess, onError) => axios.get(`${BASE_URL}/${query.product_id}/stats`)
-  .then(response => onSuccess(response.data))
-  .catch(onError)
+const getStats = (query, onSuccess, onError) => axios.get(`${BASE_URL}/products/${query.product_id}/stats`)
+  .then(({ data }) => onSuccess(data))
+  .catch(({ response }) => {
+    showAlert(response)
+    onError(response)
+  })
 
 export default {
   getProducts,
